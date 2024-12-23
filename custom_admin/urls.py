5edfb91ee7bccb6840048b861c53479cdd84811f
Raigester <vovaraigester@gmail.com>
2024-12-24 00:31:17 +0200

    refactor routes

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from custom_admin.views import (
     ApproveOrganizerView,
     CompetitionsTypeDetailView,
-    OrganizerRequestsListView,
-    UpdateEventStatusView,
     CompetitionsTypeListCreateView,
+    OrganizerRequestsListView,
     PendingEventsView,
+    UpdateEventStatusView,
 )
 
 
 urlpatterns = [
-    path('competitions-type/', CompetitionsTypeListCreateView.as_view(), name='competitions-type-list'),
-    path('competitions-type/<int:competition_type_id>/', CompetitionsTypeDetailView.as_view(),
-         name='competitions-type-detail'),
+    path('competition-type/', CompetitionsTypeListCreateView.as_view(), name='competitions-type-list'),
+    path('competition-type/<int:competition_type_id>/', CompetitionsTypeDetailView.as_view(),
+         name='competition-type-detail'),
     path('user/<int:user_id>/approve-organizer/', ApproveOrganizerView.as_view(), name='approve-organizer'),
     path('event/<int:event_id>/update-status/', UpdateEventStatusView.as_view(), name='update-event-status'),
     path('events/update-status-requests/', PendingEventsView.as_view(), name='pending-events'),
