f5465486f5307152282d143da79832f7f233ac24
Raigester <vovaraigester@gmail.com>
2024-12-29 00:59:45 +0200

    refactor routes urls

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('competition-type/<int:competition_type_id>/', CompetitionsTypeDetailView.as_view(),
          name='competition-type-detail'),
     path('user/<int:user_id>/approve-organizer/', ApproveOrganizerView.as_view(), name='approve-organizer'),
+    path('user/approve-organizer/', OrganizerRequestsListView.as_view(), name='organizer-request-list'),
     path('event/<int:event_id>/update-status/', UpdateEventStatusView.as_view(), name='update-event-status'),
-    path('event/update-status-request/', PendingEventsView.as_view(), name='pending-events'),
-    path('organizer-request/', OrganizerRequestsListView.as_view(), name='organizer-request-list'),
+    path('event/update-status/', PendingEventsView.as_view(), name='pending-events'),
 ]
